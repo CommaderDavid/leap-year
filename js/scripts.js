11f880a0c43c943a6fa15bcdac95245ec1c23a57
David Boedigheimer <stealth259@gmail.com>
2019-10-31 11:13:14 -0700

    Add script front end.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Back end
+
+// Front end
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
